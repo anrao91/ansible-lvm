d40707bcc3f52b6d1c506a2b22852dc9761f3048
Anusha Rao <anusha91rao@gmail.com>
2015-04-26 19:03:52 +0530

    Updated lv with dynamic name generation working fine
    Signed-off-by: Anusha Rao <anusha91rao@gmail.com>

--- lv.py
+++ lv.py
@@ -15,41 +15,42 @@ def lv_run_cmd(module, *args):
     options = args[3]
     data_align = args[4]
     pool_name = args[5]
+    vg_name = args[6]
+    pool2_name = args[5]
     # Create an LV
     if action == 'create':
         #format the disks to get the updated pv_list
         upvs_list = checkOutput(disks, "pv")
         #Get the updated vg_list
         ldisks = getVgList(upvs_list)
+        ldisks.sort()
         #lv_name = name_create.create(lv_name,"lv")
-        lv_name = lv_create(lv_name)
         lvcreate_cmd = module.get_bin_path('lvcreate', True)
         for ldisk in ldisks:
-            create_cmd = "%s %s %s %s"%(lvcreate_cmd, lv_name, options,
+            lv_name = lv_name_generate(lv_name,module)
+            create_cmd = "%s -n %s %s %s"%(lvcreate_cmd, lv_name, options,
                 ldisk)
             rc,output,err = module.run_command(create_cmd)
-            #if rc != 0:
-            #   module.fail_json(msg="Failed executing lv command.",rc=rc, err=err)
-            print json.dumps({ "output": output })
+            if rc != 0:
+               module.fail_json(msg="Failed executing lv command.",rc=rc, err=err)
+        module.exit_json(msg="Created lv successfully")
 
     elif action == 'convert':
         lvconvert_cmd = module.get_bin_path('lvconvert', True)
-        #Here ldisk/poolname = vgname/poolname
         #data_align = chunk size
-        #--poolmetadata = vgname/metadata
-        for ldisk in ldisks:
-            convert_cmd = "%s %s %s %s/%s %s/metadata"%(lvconvert_cmd, data_align, options,
-                ldisk, pool_name,ldisk)
-            rc,output,err = module.run_command(convert_cmd)
-            print json.dumps({ "output": output })
+        convert_cmd = "%s --chunksize %s --thinpool %s/%s --poolmetadata"
+        "%s/%s %s"%(lvconvert_cmd, data_align, vg_name,
+        pool_name,vg_name,pool2_name, options)
+        rc,output,err = module.run_command(convert_cmd)
+        if rc != 0:
+            module.fail_json(msg="Failed executing lv command.",rc=rc, err=err)
+        module.exit_json(msg="Converted lv successfully")
 
     elif action == 'change':
         lvchange_cmd = module.get_bin_path('lvchange', True)
-        for ldisk in ldisks:
-            change_cmd = "%s %s %s/%s"%(lvchange_cmd, options,
-                ldisk,pool_name)
-            rc,output,err = module.run_command(change_cmd)
-            print json.dumps({ "output": output })
+        change_cmd = "%s %s %s/%s"%(lvchange_cmd, options, pool_name)
+        rc,output,err = module.run_command(change_cmd)
+        print json.dumps({ "output": output })
 
     # Remove an LV
     elif action == 'remove':
@@ -61,6 +62,7 @@ def lv_run_cmd(module, *args):
             #    module.fail_json(msg="Failed executing lv command.",rc=rc, err=err)
 
             print json.dumps({ "output": output })
+    module.exit_json(msg="Created lv successfully")
 
 def getVgList(upvs_list):
     upvs_list = checkOutput(upvs_list,"pv")
@@ -102,7 +104,7 @@ def checkOutput(disks,command):
         ulvm_list = list(set(ulvm_list) & set(disks))
     return ulvm_list
 
-def lv_create(name):
+def lv_name_generate(name,module):
     #final_op should have vg_name or lv_name  of the current pvs_list or vgs_list passed to it
     final_op = subprocess.check_output(["vgs", "--noheadings", "-olv_name"])
 
@@ -116,31 +118,40 @@ def lv_create(name):
             number = int(num)
             number += 1
             name += str(number)
-        else:
-            name += '1'
+            #module.fail_json(msg=name+"I'm here")
+    else:
+        name += '1'
+            #module.fail_json(msg=name+"This is else")
     return name
 
 def main():
     module = AnsibleModule(
         argument_spec = dict(
-            action = dict(choices = ["create", "remove"]),
+            action = dict(choices = ["create", "remove","convert","change"]),
             disks = dict(),
             logical_vol_size = dict(type = 'str'),
             lv_name = dict(type = 'str'),
             virt_size = dict(type = 'str'),
             lv_thin = dict(type = 'str'),
-            data_align = dict(type = 'int'),
+            data_align = dict(type = 'str'),
             pool_name = dict(type = 'str'),
+            pool2_name = dict(type = 'str'),
             options = dict(type='str'),
+            vg_name = dict(type='str'),
             ),
         )
 
     action = module.params['action']
     disks = literal_eval(module.params['disks'])
-    lv_name = module.params['lvname']
+    #disks = ''
+    lv_name = module.params['lv_name']
     options = module.params['options']
-    data_align = module_params['data_align']
+    vg_name = module.params['vg_name']
+    data_align = module.params['data_align']
     pool_name = module.params['pool_name']
-    lv_run_cmd(module, action, disks, lv_name, options, data_align, pool_name)
+    pool2_name = module.params['pool2_name']
+    lv_run_cmd(module, action, disks, lv_name, options,
+        data_align, pool_name, vg_name, pool2_name)
+    sys.exit(0)
 
 main()
